7c6651b9d89babef7943c95b5e0eac14d069bb74
ngdream <nguewofoss@gmail.com>
2026-02-28 19:19:40 +0100

    https

--- api.domilix.com/app/Providers/AppServiceProvider.php
+++ api.domilix.com/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        if (app()->environment('production')) {
+            $appUrl = config('app.url');
+
+            if (!empty($appUrl)) {
+                URL::forceRootUrl($appUrl);
+            }
+
+            URL::forceScheme('https');
+        }
     }
 }
